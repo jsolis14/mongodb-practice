70f52c91db5dab51125c98443f53b534b1155f45
Jesse Solis <jesse.solis0316@gmail.com>
2020-09-09 12:49:30 -0500

    set up mocha

---
+++ test/demo_test.js
@@ -0,0 +1,10 @@
+const mocha = require('mocha');
+const assert = require('assert');
+
+describe('some demo tests', () => {
+
+    //create tests
+    it('adds two numbers together', () => {
+        assert(2 + 3 === 5)
+    })
+})
